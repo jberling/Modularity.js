c1e34509e15ab178859e8e7478cc70b6da744636
Jonasson <j.jonasson@gmail.com>
2011-05-24 16:55:14 +0200

    Updated content and site

--- scripts/articles/articles.js
+++ scripts/articles/articles.js
@@ -2,17 +2,15 @@ define([
     "text!./intro.html!strip",
     "text!./progressive-enhancement.html!strip",
     "text!./module.html!strip",
-    "text!./controller.html!strip",
     "text!./module-library.html!strip",
     "text!./change-log.html!strip",
     "text!./roadmap.html!strip"
-  ], function(intro, progressiveEnhancement, module, controller, moduleLibrary, changeLog, roadmap){
+  ], function(intro, progressiveEnhancement, module, moduleLibrary, changeLog, roadmap){
 
   return {
     intro                     : intro,
     "progressive-enhancement" : progressiveEnhancement,
     module                    : module,
-    controller                : controller,
     "module-library"          : moduleLibrary,
     "change-log"              : changeLog,
     "roadmap"                 : roadmap
