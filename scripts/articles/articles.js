dc1b2eb6b1efd10ba289fb1d5de1e461bf703aef
Jonasson <j.jonasson@gmail.com>
2011-05-25 16:50:22 +0200

    module.html content updated.

--- scripts/articles/articles.js
+++ scripts/articles/articles.js
@@ -1,15 +1,17 @@
 define([
     "text!./intro.html!strip",
     "text!./progressive-enhancement.html!strip",
+    "text!./core.html!strip",
     "text!./module.html!strip",
     "text!./module-library.html!strip",
     "text!./change-log.html!strip",
     "text!./roadmap.html!strip"
-  ], function(intro, progressiveEnhancement, module, moduleLibrary, changeLog, roadmap){
+  ], function(intro, progressiveEnhancement, core, module, moduleLibrary, changeLog, roadmap){
 
   return {
     intro                     : intro,
     "progressive-enhancement" : progressiveEnhancement,
+    core                      : core,
     module                    : module,
     "module-library"          : moduleLibrary,
     "change-log"              : changeLog,
