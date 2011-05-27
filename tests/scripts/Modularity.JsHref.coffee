28f577ecfef00b5387735c6c6ea7ef770097d27b
Jonasson <j.jonasson@gmail.com>
2011-05-27 11:40:31 +0200

    The js-href module was rewritten in CoffeeScript.

---
+++ tests/scripts/Modularity.JsHref.coffee
@@ -0,0 +1,21 @@
+define(["./Modularity"], (Modularity) ->
+
+  dataAttribute = "js-href"
+
+  Modularity.moduleDefinitions.register(
+    "js-href",
+
+    { start: (options) ->
+      clientHref = options.href
+      $(this.element).attr("href", clientHref)
+
+      unless window._jsHrefInitialized
+        $(window.document.body).delegate(
+          "click",
+          "a[data-#{ dataAttribute }]",
+          (e) -> e.preventDefault()
+        )
+    },
+
+    { dataAttribute : dataAttribute, version : "0.2.0" }
+  ))
--- tests/scripts/Modularity.JsHref.js
+++ tests/scripts/Modularity.JsHref.js
@@ -1,28 +1,21 @@
-define(["./Modularity" ], function(Modularity) {
-
-  var dataAttribute = "js-href";
-
-  var Module = Modularity.moduleDefinitions.register("js-href", {
-
-    start: function(options) {
-      var clientHref = options.href;
-      $(this.element).attr("href", clientHref);
-
-      if (!window._jsHrefInitialized) {
-        $(window.document.body).delegate(
-            "click",
-            "a[data-" + dataAttribute + "]",
-            function(e){
-              e.preventDefault();
-            });
-        window._jsHrefInitialized = true;
+(function() {
+  define(["./Modularity"], function(Modularity) {
+    var dataAttribute;
+    dataAttribute = "js-href";
+    return Modularity.moduleDefinitions.register("js-href", {
+      start: function(options) {
+        var clientHref;
+        clientHref = options.href;
+        $(this.element).attr("href", clientHref);
+        if (!window._jsHrefInitialized) {
+          return $(window.document.body).delegate("click", "a[data-" + dataAttribute + "]", function(e) {
+            return e.preventDefault();
+          });
+        }
       }
-    }
-
-  }, { dataAttribute: dataAttribute });
-
-  Module.version = "0.1.1";
-
-  return Module;
-
-});
+    }, {
+      dataAttribute: dataAttribute,
+      version: "0.2.0"
+    });
+  });
+}).call(this);
